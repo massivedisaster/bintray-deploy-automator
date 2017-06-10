e5a69cf07374c126d9a9996493359bb4216dd4bd
jzeferino <jorgevalentzeferino@gmail.com>
2017-06-10 16:16:31 +0100

    Added documentation to classes and methods.

--- src/main/java/com/massivedisaster/bintraydeployautomator/utils/GradleUtils.java
+++ src/main/java/com/massivedisaster/bintraydeployautomator/utils/GradleUtils.java
@@ -1,9 +1,20 @@
 package com.massivedisaster.bintraydeployautomator.utils;
 
 import com.sun.istack.internal.NotNull;
+
 import org.gradle.tooling.ProjectConnection;
 
+/**
+ * Gradle utilities.
+ */
 public class GradleUtils {
+    /**
+     * Runs gradle with specified parameters.
+     *
+     * @param gradleConnection the gradle connection.
+     * @param tasks            gradle tasks to execute.
+     * @param arguments        arguments.
+     */
     public static void runGradle(@NotNull ProjectConnection gradleConnection, @NotNull String[] tasks, @NotNull String... arguments) {
         gradleConnection.newBuild()
                 .forTasks(tasks)
